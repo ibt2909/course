353bef312eabbd8761325ebeb03607fa194b1508
ibt2909 <orazmerdanow06.com>
2024-07-01 00:27:57 +0500

    0.0.6

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -11,13 +11,13 @@ class DatabaseSeeder extends Seeder
     /**
      * Seed the application's database.
      */
-    public function run(): void
+    public function run()
     {
-        // User::factory(10)->create();
-
-        User::factory()->create([
-            'name' => 'Test User',
-            'email' => 'test@example.com',
-        ]);
+        $this->call(LocationsSeeder::class);
+        $this->call(ClassroomsSeeder::class);
+        $this->call(TeachersSeeder::class);
+        $this->call(StudentsSeeder::class);
+        $this->call(BranchesSeeder::class);
+        $this->call(LessonsSeeder::class);
     }
 }
