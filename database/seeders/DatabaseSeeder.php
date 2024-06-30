61e779a67ce8f61c6e6dc22e94377467adaaca98
ibt2909 <orazmerdanow06.com>
2024-07-01 00:23:18 +0500

    0.0.5

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -2,19 +2,22 @@
 
 namespace Database\Seeders;
 
+use App\Models\User;
+// use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 
 class DatabaseSeeder extends Seeder
 {
-    public function run()
+    /**
+     * Seed the application's database.
+     */
+    public function run(): void
     {
-        \App\Models\Location::factory(3)->create();
-        \App\Models\Classroom::factory(10)->create();
-        \App\Models\Teacher::factory(10)->create();
-        \App\Models\Student::factory(50)->create();
-        \App\Models\Branch::factory(5)->create();
-        \App\Models\Lesson::factory(50)->create();
-        \App\Models\Registration::factory(100)->create();
+        // User::factory(10)->create();
+
+        User::factory()->create([
+            'name' => 'Test User',
+            'email' => 'test@example.com',
+        ]);
     }
 }
-
